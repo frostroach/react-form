0dc75e80a9b9380e17303959dcdf0a9db592eebb
Vitor Cesar <vitor@fasters.com.br>
2022-08-11 12:33:47 -0300

    feat: implement helpers

---
+++ src/utils/helpers/generateCheckSums.ts
@@ -0,0 +1,19 @@
+type CheckSums = [number, number];
+
+const generateCheckSums = (
+  numbers: Array<number>,
+  validators: Array<number>,
+): CheckSums => {
+  const initialCheckSums: CheckSums = [0, 0];
+
+  return validators.reduce(
+    ([checkerA, checkerB], validator, index) =>
+      [
+        index === 0 ? 0 : checkerA + numbers[index - 1] * validator,
+        checkerB + numbers[index] * validator,
+      ] as CheckSums,
+    initialCheckSums,
+  );
+};
+
+export default generateCheckSums;
