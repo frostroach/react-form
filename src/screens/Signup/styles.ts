ad8a8b9378023b5d7891be2a5b9577c0f50994c6
Vitor Cesar <vitor@fasters.com.br>
2022-08-18 12:48:26 -0300

    feat: add signup flow to user

--- src/screens/Signup/styles.ts
+++ src/screens/Signup/styles.ts
@@ -1,8 +1,25 @@
 import styled from "styled-components";
 import { Link as ReactLink } from "react-router-dom";
+import { Button, Grid } from "@mui/material";
 
 const Link = styled(ReactLink)`
   text-decoration: none;
 `;
 
-export const SignupStyled = { Link };
+const HeaderContainer = styled(Grid)`
+  align-content: center;
+  display: flex;
+  flex-direction: row;
+  padding: 0 1.5em;
+  margin-bottom: 1em;
+  justify-content: space-between;
+`;
+
+const ReturnButton = styled(Button).attrs({
+  sx: {
+    minWidth: 0,
+    padding: "4px 10px",
+  },
+})``;
+
+export const SignupStyled = { ReturnButton, HeaderContainer, Link };
