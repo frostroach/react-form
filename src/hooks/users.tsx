e82028321b64886e2bf733bc4ba002141c8b2a36
Vitor Cesar <vitor@fasters.com.br>
2022-08-22 11:35:08 -0300

    feat: implement delete user

--- src/hooks/users.tsx
+++ src/hooks/users.tsx
@@ -3,6 +3,7 @@ import { User } from "../models/user";
 
 type UserProps = {
   addUser: (userData: User) => void;
+  deleteUser: (userId: string) => void;
   users: User[];
 };
 
@@ -13,17 +14,39 @@ type UserProviderProps = {
 const UserContext = createContext<UserProps>({} as UserProps);
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([
+    {
+      cpf: "12345678911",
+      id: "712838718jjx",
+      name: "Vitor",
+      surname: "Testes",
+      newsletter: true,
+      sales: true,
+    },
+  ]);
 
-  const addUser = (userData: User): void => {
-    const findUser = users.find((user) => user.id === userData.id);
-    if (!findUser) {
-      setUsers([...users, userData]);
+  const addUser = async (userData: User): Promise<void> => {
+    try {
+      const findUser = users.find((user) => user.cpf === userData.cpf);
+      if (findUser) {
+        // eslint-disable-next-line no-throw-literal
+        throw "Há um usuário com este CPF!";
+      }
+      if (!findUser) {
+        setUsers([...users, userData]);
+      }
+    } catch (err: any) {
+      throw new Error(err);
     }
   };
 
+  const deleteUser = (userId: string): void => {
+    const updatedUsers = users.filter((user) => user.id !== userId);
+    setUsers(updatedUsers);
+  };
+
   return (
-    <UserContext.Provider value={{ users, addUser }}>
+    <UserContext.Provider value={{ users, addUser, deleteUser }}>
       {children}
     </UserContext.Provider>
   );
