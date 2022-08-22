0fc2970561ad5c12858659f551931d7886519114
Vitor Cesar <vitor@fasters.com.br>
2022-08-22 11:34:51 -0300

    feat: add tabs to app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BasicTabs from "../components/Tabs";
+
 import Signup from "../screens/Signup";
 import Users from "../screens/Users";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
+      <BasicTabs />
       <Routes>
         <Route path="/" element={<Users />} />
         <Route path="/signup" element={<Signup />} />
